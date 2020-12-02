565d5596497e33f35ae97877ecdfb7e4822a6a8a
Callum Duffy <callum.duffy@jpmchase.com>
2020-12-02 14:49:44 +0000

    part two done

--- day_two/password_philosophy.py
+++ day_two/password_philosophy.py
@@ -36,19 +36,34 @@ def simple_solve():
     policy_list = fetch_input_as_list()
     count = 0
     for policy in policy_list:
-        if policy.is_valid_password():
+        if policy.is_valid_password_p1():
             count+=1
     return count
+
 
 def better_solve():
     """Improve it by processing each line as it goes."""
     correct_passwords = 0
-
     try:
         file_content = open(INPUT_FILE, "r")
         for line in file_content.read().split('\n'):
             pp = split_input_line(line)
-            if pp.is_valid_password():
+            if pp.is_valid_password_p1():
+                correct_passwords+=1
+        return correct_passwords
+    except Exception as ex:
+        print(f"Issue reading input from file: {ex}")
+        raise ValueError("Issue reading input from file.")
+
+
+def second_star_solve():
+    """Changing to solve the second star."""
+    correct_passwords = 0
+    try:
+        file_content = open(INPUT_FILE, "r")
+        for line in file_content.read().split('\n'):
+            pp = split_input_line(line)
+            if pp.is_valid_password_p2():
                 correct_passwords+=1
         return correct_passwords
     except Exception as ex:
@@ -63,5 +78,13 @@ def run_part_one():
     better_ans = better_solve()
     print(better_ans)
 
+
+def run_part_two():
+    """Run all functions to solve part two of the problem."""
+    p2_ans = second_star_solve()
+    print(p2_ans)
+
+
 if __name__ == "__main__":
     run_part_one()
+    run_part_two()
--- day_two/password_policy.py
+++ day_two/password_policy.py
@@ -10,7 +10,7 @@ class PasswordPolicy:
         self.character = character
         self.password = password
 
-    def is_valid_password(self):
+    def is_valid_password_p1(self):
         """Check if the password fits its rules."""
         char_count = 0
         for char in self.password:
@@ -18,6 +18,13 @@ class PasswordPolicy:
                 return False
             if char == self.character:
                 char_count+=1
-        
         if char_count <= self.maximum and char_count >= self.minimum:
             return True
+
+    def is_valid_password_p2(self):
+        """Check if the password is valid for part two."""
+        if self.password[self.minimum-1] == self.character and self.password[self.maximum-1] == self.character:
+            return False
+        elif self.password[self.minimum-1] == self.character or self.password[self.maximum-1] == self.character:
+            return True
+        return False
